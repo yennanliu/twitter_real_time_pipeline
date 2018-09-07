c592824790b393debaa36248166881aea11ad678
yennanliue <f339339@gmail.com>
2018-09-07 11:54:19 +0100

    fix dag args to make etl_app_V3 dag run OK

--- dags/etl_app_V3.py
+++ dags/etl_app_V3.py
@@ -7,22 +7,13 @@ from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
 
 
-default_args = {
+args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2015, 6, 1),
-    'email': ['airflow@example.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-    # 'queue': 'bash_queue',
-    # 'pool': 'backfill',
-    # 'priority_weight': 10,
-    # 'end_date': datetime(2016, 1, 1),
+    'start_date': datetime.now()
 }
 
-dag = DAG('tutorial', default_args=default_args)
+dag = DAG('etl_app_V3', default_args=args)
 
 # t1, t2 and t3 are examples of tasks created by instantiating operators
 t1 = BashOperator(
