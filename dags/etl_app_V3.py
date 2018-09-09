1acc11e8d2784c6289a6bad5221809fdfaa08528
yennanliue <f339339@gmail.com>
2018-09-09 09:12:45 +0100

    modify V3 etl to scrape  twitter via api

--- dags/etl_app_V3.py
+++ dags/etl_app_V3.py
@@ -4,6 +4,7 @@ https://github.com/apache/incubator-airflow/blob/master/airflow/example_dags/tut
 """
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator, BranchPythonOperator
 from datetime import datetime, timedelta
 
 
@@ -13,33 +14,59 @@ args = {
     'start_date': datetime.now()
 }
 
-dag = DAG('etl_app_V3', default_args=args)
+#dag = DAG('etl_app_V3', default_args=args)
 
-# t1, t2 and t3 are examples of tasks created by instantiating operators
-t1 = BashOperator(
-    task_id='print_date',
-    bash_command='date',
-    dag=dag)
 
-t2 = BashOperator(
-    task_id='sleep',
-    bash_command='sleep 5',
-    retries=3,
-    dag=dag)
 
-templated_command = """
-    {% for i in range(5) %}
-        echo "{{ ds }}"
-        echo "{{ macros.ds_add(ds, 7)}}"
-        echo "{{ params.my_param }}"
-    {% endfor %}
-"""
+# -----------------------------------
+# job func 
 
-t3 = BashOperator(
-    task_id='templated',
-    bash_command=templated_command,
-    params={'my_param': 'Parameter I passed in'},
-    dag=dag)
+from twython import Twython
+import time
+import os 
 
-t2.set_upstream(t1)
-t3.set_upstream(t1)
+try:
+    APP_KEY = os.environ['APP_KEY']
+    APP_SECRET = os.environ['APP_SECRET'] 
+except:
+    print (' No API key , please set up  via : ')
+    print (' https://developer.twitter.com/en/apps')
+
+def main():
+    twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
+    ACCESS_TOKEN = twitter.obtain_access_token()
+    print (ACCESS_TOKEN)          
+    ACCESS_TOKEN = ACCESS_TOKEN
+    twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
+    twitter.get_application_rate_limit_status()['resources']['search']
+    #RETRIEVING REAL TIME STREAMING TWEETS ABOUT BLOCKCHAIN 
+    search = twitter.search(q="blockchain", count=2000)
+    tweets = search['statuses']
+    #for tweet in tweets:
+    #print (tweet['id_str'], '\n', tweet['text'], tweet['favorite_count'], tweet['retweet_count'] ), '\n\n\n'
+    ids = []
+    #for tweet in tweets:
+    #ids.append(tweet['id_str'])
+    ids = [tweet['id_str'] for tweet in tweets]
+    texts = [tweet['text'] for tweet in tweets]
+    times = [tweet['retweet_count'] for tweet in tweets]
+    favtimes = [tweet['favorite_count'] for tweet in tweets]
+    follower_count = [tweet['user']['followers_count'] for tweet in tweets]
+    location = [tweet['user']['location'] for tweet in tweets]
+    lang = [tweet['lang'] for tweet in tweets]
+    print (tweets[0])
+    return tweets
+
+# -----------------------------------
+
+
+
+
+with DAG('etl_app_v3', default_args=args) as dag:
+    superman_task = PythonOperator(
+        task_id='main',
+        python_callable=main
+    )
+
+
+
