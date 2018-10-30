68c2b0f733111f104464c985a1ebdf7b7d5cbc52
yennanliue <f339339@gmail.com>
2018-10-30 08:49:50 +0000

    fix main load func name

--- load_creds.py
+++ load_creds.py
@@ -11,7 +11,7 @@ consumer_key = config['twitter_api']['consumer_key']
 consumer_secret = config['twitter_api']['consumer_secret'] 
 
 
-def get_spotify_client_id_secret():
+def get_twitter_api_secret():
 	print (' access_token = ', access_token)
 	print (' access_token_secret = ', access_token_secret)
 	print (' consumer_key = ', consumer_key)
