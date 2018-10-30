e91880e7445de866d1b0ee13208fd91defefb464
yennanliue <f339339@gmail.com>
2018-10-30 08:45:43 +0000

    remove depreciated creds

--- load_creds.py
+++ load_creds.py
@@ -9,8 +9,6 @@ access_token = config['twitter_api']['access_token']
 access_token_secret = config['twitter_api']['access_token_secret'] 
 consumer_key = config['twitter_api']['consumer_key']
 consumer_secret = config['twitter_api']['consumer_secret'] 
-APP_KEY = config['twitter_api']['APP_KEY']
-APP_SECRET = config['twitter_api']['APP_SECRET'] 
 
 
 def get_spotify_client_id_secret():
@@ -18,6 +16,4 @@ def get_spotify_client_id_secret():
 	print (' access_token_secret = ', access_token_secret)
 	print (' consumer_key = ', consumer_key)
 	print (' consumer_secret = ', consumer_secret)
-	print (' APP_KEY = ', APP_KEY)
-	print (' APP_SECRET = ', APP_SECRET)
-	return access_token, access_token_secret, consumer_key, consumer_secret, APP_KEY, APP_SECRET
+	return access_token, access_token_secret, consumer_key, consumer_secret
