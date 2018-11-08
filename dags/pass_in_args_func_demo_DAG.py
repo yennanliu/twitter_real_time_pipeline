f88ef5efb6847483576182cade29fac46985300c
yennanliu <f339339@gmail.com>
2018-11-08 13:03:58 +0000

    add DAG demo pass args to python func

---
+++ dags/pass_in_args_func_demo_DAG.py
@@ -0,0 +1,40 @@
+# python 3 
+# Passing in arguments
+# https://airflow.readthedocs.io/en/latest/howto/operator.html
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.dummy_operator import DummyOperator
+import datetime
+
+
+args = {
+'owner': 'yen',
+'depends_on_past': False,
+'start_date': datetime.datetime.now()}
+
+
+
+
+def my_sleeping_function(random_base):
+    """This is a function that will run within the DAG execution"""
+    print (' random_base : ',  random_base)
+    time.sleep(random_base)
+
+
+
+with DAG('pass_in_args_func_demo_DAG', default_args=args) as dag:
+	run_this = DummyOperator(task_id='run_this')
+	# Generate 10 sleeping tasks, sleeping from 0 to 4 seconds respectively
+	for i in range(5):
+		task = PythonOperator(
+		    task_id='sleep_for_' + str(i),
+		    python_callable=my_sleeping_function,
+		    op_kwargs={'random_base': float(i) / 10},
+		    dag=dag,
+		)
+
+		run_this >> task
+
+
+
